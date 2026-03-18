8d031fbc0d1cce52862726544291c3e334dc50db
JiyounRoth <j.stella.hong@gmail.com>
2026-03-18 12:02:28 +0200

    Refactor NCO test to include enable signal and clock cycles

--- tb/nco/test_nco100.py
+++ tb/nco/test_nco100.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import Timer, RisingEdge
+from cocotb.triggers import ClockCycles, RisingEdge
 
 from tb.fixedpoint import FixedPoint
 from tb.tb_plot_utils import PlotUtils
@@ -10,27 +10,28 @@ from tb.tb_plot_utils import PlotUtils
 @cocotb.test()
 async def test_nco100(dut):
 
-    cocotb.start_soon(Clock(dut.clk,10,units='ns').start())
+    cocotb.start_soon(Clock(dut.clk,10,unit='ns').start())
 
     dut.phase_inc.value = 1288490188
+    dut.en.value = 0
     num_of_samples = 2048
     sin_vals = [0]*num_of_samples
     cos_vals = [0]*num_of_samples
     # 2 clk Reset
     dut.rst.value = 1
-    await RisingEdge(dut.clk)
-    await RisingEdge(dut.clk)
+    await ClockCycles(dut.clk,5)
     dut.rst.value = 0
+    dut.en.value = 1
     await RisingEdge(dut.clk)
     
     
-    # 1 clk latency
-    await RisingEdge(dut.clk) 
-    
+    # 2-cycle latency
+    await ClockCycles(dut.clk,2) 
+
     for i in range (num_of_samples):
         
-        sin_val = FixedPoint.q1_15_to_float(dut.sin_out.value.signed_integer)
-        cos_val = FixedPoint.q1_15_to_float(dut.cos_out.value.signed_integer)
+        sin_val = FixedPoint.q1_15_to_float(dut.sin_out.value.to_signed())
+        cos_val = FixedPoint.q1_15_to_float(dut.cos_out.value.to_signed())
         dut._log.info(f"Sample = {i}, sin_val = {sin_val}, cos_val = {cos_val} ")            
         sin_vals[i] = sin_val
         cos_vals[i] = cos_val
@@ -38,6 +39,6 @@ async def test_nco100(dut):
         await RisingEdge(dut.clk)
 
             
-    #Plot in time domain
+    # Plot
     PlotUtils.plot_time_freq(100e6, sin_vals, cos_vals, num_of_samples, "nco100")
-   
+  
