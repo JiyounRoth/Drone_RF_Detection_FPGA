960ee53b052baba1101ccc9217a6bab62fb44a74
JiyounRoth <j.stella.hong@gmail.com>
2026-03-18 12:01:12 +0200

    Enhance FFT plotting with dB scale and subplots
    Updated FFT plotting to include dB scale and improved layout.

--- tb/tb_plot_utils.py
+++ tb/tb_plot_utils.py
@@ -27,16 +27,28 @@ class PlotUtils:
         fft_cos = np.fft.fftshift(np.fft.fft(cos_vals)/N)
         shifted_freqs = np.fft.fftshift(freqs)
 
-        plt.figure(figsize=(10,5))
-        plt.plot(shifted_freqs/1e6, np.abs(fft_sin), "b--", label = "sin")
-        plt.plot(shifted_freqs/1e6, np.abs(fft_cos), "r--", label = "cos")
-        plt.title(f"FFT of {filename} sin/cos")
-        plt.xlabel("Frequency (MHz)")
-        plt.ylabel("Normalized Magnitude")
-        plt.grid()
-        plt.legend()
-        plt.savefig(f"{filename}_sin_cos_fft.png")
-        plt.close()
+        fig, axs = plt.subplots(2,1, figsize = (10,10))
+        
+        
+        axs[0].plot(shifted_freqs/1e6, np.abs(fft_sin), "b--", label = "sin")
+        axs[0].plot(shifted_freqs/1e6, np.abs(fft_cos), "r--", label = "cos")
+        axs[0].set_title(f"FFT of {filename} sin/cos")
+        axs[0].set_xlabel("Frequency (MHz)")
+        axs[0].set_ylabel("Normalized Magnitude")
+        axs[0].grid()
+        axs[0].legend()
+        
+                                        # add a tiny offset to prevent log10(0)
+        axs[1].plot(shifted_freqs/1e6, 20*np.log10(np.abs(fft_sin)+ 1e-12), "b--", label = "sin [dB]") 
+        axs[1].plot(shifted_freqs/1e6, 20*np.log10(np.abs(fft_cos)+ 1e-12), "r--", label = "cos [dB]")
+        axs[1].set_title(f"FFT(dB) of {filename} sin/cos")
+        axs[1].set_xlabel("Frequency (MHz)")
+        axs[1].set_ylabel("Magnitude (dB)")
+        axs[0].grid()
+        axs[0].legend()
+        
+        fig.savefig(f"{filename}_sin_cos_fft.png")
+        plt.close(fig)
     
 
     def plot_time_freq(fs,sin_vals, cos_vals, num_of_samples, filename):
@@ -44,4 +56,3 @@ class PlotUtils:
         x_time= np.arange(num_of_samples)/fs
         PlotUtils.plot_time_domain(x_time,sin_vals, cos_vals, filename )
         PlotUtils.plot_fft(fs, sin_vals, cos_vals, filename)
-        
